a4d827dbfa969221258564256ac2e82d283cc4d3
Namfah01 <kanokwanbing09@gmail.com>
2024-05-14 14:19:59 +0700

    ex1

---
+++ intro_functions.js
@@ -0,0 +1,29 @@
+
+function add(a, b) {
+    return a+b;
+}
+
+function substract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+console.log(add(1,2));
+console.log(substract(1,2));
+console.log(multiply(2,4));
+console.log(devide(4,8));
+
+
+
+function sayHi(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby + ".");
+}
+
+sayHi("Namfah", "Bangkok", "sleeping");
